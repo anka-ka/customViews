0e29a5a919d4715ffa58753403459ceb24f925b1
kaz <kazan19066@gmail.com>
2024-09-18 16:34:32 +0300

    customView added

---
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.statsview.ui.StatsView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+       findViewById<StatsView>(R.id.startsView).data = listOf(
+           0.25F,
+           0.25F,
+       )
+    }
+}
