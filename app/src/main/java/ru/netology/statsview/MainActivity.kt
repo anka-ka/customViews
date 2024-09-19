66b820b70df6387a20c79775cc4893fe6e65bd01
kaz <kazan19066@gmail.com>
2024-09-19 17:13:56 +0300

    transition

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,11 +1,15 @@
 package ru.netology.statsview
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Scene
+import androidx.transition.TransitionManager
 import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
@@ -22,19 +26,27 @@ class MainActivity : AppCompatActivity() {
 
         )
 
+        val root = findViewById<ViewGroup>(R.id.root)
+
+        val scene = Scene.getSceneForLayout(root, R.layout.end_scene, this)
+
+        findViewById<View>(R.id.goButton).setOnClickListener{
+            TransitionManager.go(scene)
+        }
+
         val textView = findViewById<TextView>(R.id.label)
 
         val viewAnim = AnimationUtils.loadAnimation(this, R.anim.animation).apply {
                 setAnimationListener(object: Animation.AnimationListener{
-                    override fun onAnimationStart(p0: Animation?) {
+                    override fun onAnimationStart(animation: Animation?) {
                         textView.text = "onAnimationStart"
                     }
 
-                    override fun onAnimationEnd(p0: Animation?) {
+                    override fun onAnimationEnd(animation: Animation?) {
                         textView.text = "onAnimationEnd"
                     }
 
-                    override fun onAnimationRepeat(p0: Animation?) {
+                    override fun onAnimationRepeat(animation: Animation?) {
                         textView.text = "onAnimationRepeat"
                     }
 
